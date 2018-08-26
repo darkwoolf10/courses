507dec7165f6dbd98d0a558602a04ad377acc8cb
darkwoolf10 <woolftreuser@ukr.net>
2018-08-26 21:11:45 +0300

    Добавил выбор героев

--- App.py
+++ App.py
@@ -6,10 +6,10 @@ import pygame
 from itertools import tee
 
 class App():
-    def __init__(self):
+    def __init__(self, hero):
         self.background = pygame.image.load(BACKGROUND)
-        self.hero = Turtle(x, y, width, height, (0, 255, 0))
         self.lines = []
+        self.hero = hero
         self.win = pygame.display.set_mode((500, 500))
         self.run = True
 
@@ -22,7 +22,7 @@ class App():
         global command, x, y, run, pen, turtleImage
         command = input("Insert new command: ")
         if command == "exit":
-            print("Good bye")
+            print("\033[1;44mGood bye\033[1;m")
             self.run = False
         if command.startswith("fd "):
             destination = int(command[3:])
@@ -30,15 +30,22 @@ class App():
             self.hero.y += -destination * math.cos(math.radians(self.hero.angle))
         if command.startswith("bk "):
             destination = int(command[3:])
-            self.hero.rotate(180)
-            self.hero.x += -destination * math.sin(math.radians(self.hero.angle))
-            self.hero.y += -destination * math.cos(math.radians(self.hero.angle))
+            # self.hero.rotate(180)
+            self.hero.x -= -destination * math.sin(math.radians(self.hero.angle))
+            self.hero.y -= -destination * math.cos(math.radians(self.hero.angle))
         if command.startswith("lt "):
             angle = int(command[3:])
             self.hero.rotate(angle)
         if command.startswith("rt "):
             angle = int(command[3:])
             self.hero.rotate(-angle)
+        if command == "help":
+            print("\033[1;32mfd n\033[1;m  - Вперед на n пікселів.")
+            print("\033[1;32mbk n\033[1;m  - Назад на n пікселів.")
+            print("\033[1;32mlt n\033[1;m  - Поворот вліво на n пікселів.")
+            print("\033[1;32mrt n\033[1;m  - Поворот вправо на n пікселів.")
+            print("\033[1;32mpen()\033[1;m - Почати/припинити малювати лінію.")
+            print("\033[1;32mexit\033[1;m  - Вихід з гри.\n")
         if command == "pen()":
             if self.hero.pen == False:
                 self.hero.pen = True
--- main.py
+++ main.py
@@ -2,13 +2,19 @@
 import pygame
 from settings import *
 from App import App
+from Turtle import Turtle
+from BestTurtle import BestTurtle
 
 pygame.init()
 pygame.display.set_caption(TITLE)
 clock = pygame.time.Clock()
 
-app = App()
 if __name__ == "__main__":
+    turtle = input("Select turtle 1 or 2:")
+    if turtle == "1":
+        app = App(Turtle(x, y, width, height, (0, 255, 0)))
+    elif turtle == "2":
+        app = App(BestTurtle(x, y, width, height, (0, 255, 0)))
     while app.run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
